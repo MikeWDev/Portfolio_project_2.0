d06a0197aee06305431ca019c0ac0f43e70f90eb
MikeWDev <130941015+MikeWDev@users.noreply.github.com>
2024-03-06 11:08:36 +0100

    Finished frontedn for add project panel

--- admin/src/Pages/AddProjectPanel/ImageInput.jsx
+++ admin/src/Pages/AddProjectPanel/ImageInput.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { FileImage } from "@phosphor-icons/react";
-
+import dummyimg from "../../assets/user-img.jpg";
 const ImageInput = () => {
+  const [selectedImg, setSelectedImg] = useState(null);
   return (
     <div className="image-input">
-      <input type="file" />
-      <FileImage size={48} color="#ada9bb" />
+      {selectedImg ? (
+        <div className="img-field">
+          <img src={selectedImg} alt="" />
+          <span
+            onClick={() => {
+              setSelectedImg(null);
+            }}
+          >
+            Remove
+          </span>
+        </div>
+      ) : (
+        <div className="file-input">
+          <FileImage size={48} color="#ada9bb" />
+          <input
+            onChange={(e) => {
+              const img = URL.createObjectURL(e.target.files[0]);
+              setSelectedImg(img);
+            }}
+            type="file"
+          />
+        </div>
+      )}
     </div>
   );
 };
