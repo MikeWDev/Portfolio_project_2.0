79195e95407503aca81eb79ed4d6c35fa1de4a10
MikeWDev <130941015+MikeWDev@users.noreply.github.com>
2024-05-28 14:34:06 +0200

    Changed cv file

--- frontend/src/Pages/HeroPage/CvDownload.jsx
+++ frontend/src/Pages/HeroPage/CvDownload.jsx
@@ -1,10 +1,10 @@
 import { DownloadSimple } from "@phosphor-icons/react";
 import React from "react";
-import cv from "../../assets/Components/Michał_Kocik_CV.pdf";
+import cv from "../../assets/Components/Michal_Kocik_CV_EN.pdf";
 const CvDownload = () => {
   return (
     <div className="cv-download">
-      <a href={cv} download="Michał_Kocik_CV.pdf">
+      <a href={cv} download="Michał_Kocik_CV_EN.pdf">
         <div className="text">
           CV
           <div className="svg">
