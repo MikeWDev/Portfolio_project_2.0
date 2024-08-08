a13ca06d071f5f43cc71c8e94ff25702af064dcf
MikeWDev <130941015+MikeWDev@users.noreply.github.com>
2024-08-08 21:14:09 +0200

    FB v4

--- frontend/functions/index.js
+++
@@ -1,8 +0,0 @@
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-const functions = require("firebase-functions");
-const express = require("express");
-const app = require("../server/server");
-
-exports.api = functions.https.onRequest(app);
